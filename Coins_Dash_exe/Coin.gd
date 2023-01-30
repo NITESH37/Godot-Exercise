19d5ae2d4020be152c678293e2dc86793f229471
Nitesh <niteshkumarpandit37@gmail.com>
2023-01-30 22:21:14 +0530

    add obstacles

--- Coins_Dash_exe/Coin.gd
+++ Coins_Dash_exe/Coin.gd
@@ -3,10 +3,14 @@ extends Area2D
 
 var screensize
 
+func _on_Coin_area_entered(area):
+	if area.is_in_group("obstacles"):
+		position = Vector2(rand_range(0, screensize.x), rand_range(0, screensize.y))
+
 
 func pickup():
 	queue_free()
 
 
 func _ready() -> void:
-	pass
+	connect("area_entered", self, "_on_Coin_area_entered")
