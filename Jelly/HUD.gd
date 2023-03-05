87f133c7b8df61a4da21bd43e9be7ef0564b0e13
Nitesh <niteshkumarpandit37@gmail.com>
2023-03-05 11:13:51 +0530

    initial game setup

---
+++ Jelly/HUD.gd
@@ -0,0 +1,35 @@
+extends CanvasLayer
+
+
+signal start_game
+onready var start_btn = $Start
+
+
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+
+
+func show_game_over():
+	$Message.show()
+	$Start.show()
+	$Score.show()
+
+
+func _on_messageTimer_timeout():
+	$Message.hide()
+
+
+func _on_StartButton_pressed():
+	$Message.hide()
+	$Start.hide()
+	emit_signal("start_game")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	start_btn.connect("pressed",self, "_on_StartButton_pressed")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
